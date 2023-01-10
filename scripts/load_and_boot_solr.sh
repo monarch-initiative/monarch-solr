69dd997bb52f9d69e767e29bdec6e23c8e1120f0
Kevin Schaper <kevinschaper@gmail.com>
2023-01-09 18:38:26 -0800

    fixed file permissions, path to script, still not working

--- scripts/load_and_boot_solr.sh
+++ scripts/load_and_boot_solr.sh
@@ -3,7 +3,7 @@
 SOLR_DUMP_FILENAME="solr.tar.gz"
 
 [ ! -z ${DO_LOAD} ] && \
-    echo "Loading Solr dump file '$SOLR_DUMP_FILENAME" 
-    scripts/download_solr.sh
+    echo "Loading Solr dump file '$SOLR_DUMP_FILENAME'" 
+    /scripts/download_solr.sh
 
 tini -s -g -- /opt/solr/docker-entrypoint.sh 
